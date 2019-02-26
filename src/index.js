60d48ee9933b18641908fb9b4ca4a070300fc2d3
rbarakhvostov <rbarakhvostov@gmail.com>
2019-02-26 17:34:07 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function makeExchange(currency) {
-  if (currency === 0) return {};
+  if (currency <= 0) return {};
   if (currency > 10000 ) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
   let coins = {}, h = 50, q = 25, d = 10, n = 5, p = 1; 
   if (currency >= h) coins.H = Math.floor(currency / h), currency = currency % h;
